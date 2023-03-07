01440eea0466643e7defeef9b151858f2a23d332
марта <kukovamk@gmail.com>
2023-03-07 10:03:51 +0300

    HW_1

--- HW_2/2_1/Program.cs
+++ HW_2/2_1/Program.cs
@@ -3,8 +3,16 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.WriteLine("Введите трехзначное число")!;
+Console.WriteLine("Введите число");
 
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num / 10 % 10;
+}
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
 
  
 
